647a05050fb1f68d0b4142aee7e516813ebb85ba
MikeLiuty <mikeliuty@gmail.com>
2017-11-23 16:25:10 +1300

    News Block updated

--- src/js/component/pc_newscontainer.js
+++ src/js/component/pc_newscontainer.js
@@ -5,6 +5,7 @@ import car_1 from '../../images/carousel_1.jpg';
 import car_2 from '../../images/carousel_2.jpg';
 import car_3 from '../../images/carousel_3.jpg';
 import car_4 from '../../images/carousel_4.jpg';
+import PCNewsBlock from './pc_news_block'
 
 const TabPane = Tabs.TabPane;
 export default class PCNewsContainer extends React.Component{
@@ -22,7 +23,7 @@ export default class PCNewsContainer extends React.Component{
         <div>
           <Row>
             <Col span={2}></Col>
-            <Col span={20} className="container">
+            <Col span={16} className="container">
               <div className="leftContainer">
                 <div class="carousel">
                   <Carousel {...setting}>
@@ -33,8 +34,13 @@ export default class PCNewsContainer extends React.Component{
                   </Carousel>
                 </div>
               </div>
+              <Tabs className='tabs_news'>
+                <TabPane tab="news" key="1">
+                  <PCNewsBlock count={22} type="top" width="100%" bordered="false" />
+                </TabPane>
+              </Tabs>
             </Col>
-            <Col span={2}></Col>
+            <Col span={4}></Col>
           </Row>
         </div>
       );
