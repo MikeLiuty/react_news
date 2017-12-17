a4c8a3e38d722d5dff4d9e4ee4eb533b8f919ea4
Mikeliuty <mikeliuty@gmail.com>
2017-12-18 10:14:05 +1300

    SmallPics

--- src/js/component/pc_newscontainer.js
+++ src/js/component/pc_newscontainer.js
@@ -6,6 +6,7 @@ import car_2 from '../../images/carousel_2.jpg';
 import car_3 from '../../images/carousel_3.jpg';
 import car_4 from '../../images/carousel_4.jpg';
 import PCNewsBlock from './pc_news_block'
+import PCNewsImageBlock from './pc_news_image_block'
 
 const TabPane = Tabs.TabPane;
 export default class PCNewsContainer extends React.Component{
@@ -25,7 +26,7 @@ export default class PCNewsContainer extends React.Component{
             <Col span={2}></Col>
             <Col span={16} className="container">
               <div className="leftContainer">
-                <div class="carousel">
+                <div className="carousel">
                   <Carousel {...setting}>
                     <div><img src={car_1}/></div>
                     <div><img src={car_2}/></div>
@@ -33,10 +34,15 @@ export default class PCNewsContainer extends React.Component{
                     <div><img src={car_4}/></div>
                   </Carousel>
                 </div>
+                {/* <PCNewsImageBlock count={6} type="international" width="400px" cartTitle="internationalTop" imageWidth="112px"/> */}
+                <PCNewsImageBlock count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="112px"/>
               </div>
               <Tabs className='tabs_news'>
                 <TabPane tab="news" key="1">
                   <PCNewsBlock count={22} type="top" width="100%" bordered="false" />
+                </TabPane>
+                <TabPane tab="International" key="2">
+                  <PCNewsBlock count={22} type="international" width="100%" bordered="false" />
                 </TabPane>
               </Tabs>
             </Col>
